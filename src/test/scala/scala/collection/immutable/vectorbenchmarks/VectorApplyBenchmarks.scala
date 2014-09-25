ef6a4ada800708b580cd23f5fd8408ace4b2ec56
nicolasstucki <nicolas.stucki@gmail.com>
2014-09-25 11:09:50 +0200

    Added pseudo-random number generator for benchmarks

--- src/test/scala/scala/collection/immutable/vectorbenchmarks/VectorApplyBenchmarks.scala
+++ src/test/scala/scala/collection/immutable/vectorbenchmarks/VectorApplyBenchmarks.scala
@@ -53,20 +53,39 @@ class VectorApplyBenchmarks extends PerformanceTest.OfflineRegressionReport with
                 }
             }
 
-            performance of "1M random indices" in {
-                def benchmarkFunction(vec: Seq[Int]) = {
-                    var sum = 0
-                    val len = vec.length
-                    for (d <- RandomDataSet.getDoubles(1000000)) {
-                        val i = (d * len).toInt
-                        sum += vec.apply(i)
-                    }
-                    sideeffect = sum
+            def benchmarkFunctionPseudoRandom(vec: Seq[Int], seed: Int) = {
+                val rnd = new scala.util.Random(seed)
+                var i = 0
+                var sum = 0
+                val len = vec.length
+                while (i < 1000000) {
+                    sum += vec.apply(rnd.nextInt(len))
+                    i += 1
                 }
+                sideeffect = sum
+            }
+
+            performance of "1M pseudo-random indices (seed=42)" in {
                 performance of s"Height $height" in {
-                    using(vectors(from, to, by)) curve ("Vector") in (benchmarkFunction _)
-                    using(rbvectors(from, to, by)) curve ("rbVector") in (benchmarkFunction _)
-                    using(rrbvectors(from, to, by)) curve ("rrbVector") in (benchmarkFunction _)
+                    using(vectors(from, to, by)) curve ("Vector") in (benchmarkFunctionPseudoRandom(_, 42))
+                    using(rbvectors(from, to, by)) curve ("rbVector") in (benchmarkFunctionPseudoRandom(_, 42))
+                    using(rrbvectors(from, to, by)) curve ("rrbVector") in (benchmarkFunctionPseudoRandom(_, 42))
+                }
+            }
+
+            performance of "1M pseudo-random indices (seed=274181)" in {
+                performance of s"Height $height" in {
+                    using(vectors(from, to, by)) curve ("Vector") in (benchmarkFunctionPseudoRandom(_, 274181))
+                    using(rbvectors(from, to, by)) curve ("rbVector") in (benchmarkFunctionPseudoRandom(_, 274181))
+                    using(rrbvectors(from, to, by)) curve ("rrbVector") in (benchmarkFunctionPseudoRandom(_, 274181))
+                }
+            }
+
+            performance of "1M pseudo-random indices (seed=53426)" in {
+                performance of s"Height $height" in {
+                    using(vectors(from, to, by)) curve ("Vector") in (benchmarkFunctionPseudoRandom(_, 53426))
+                    using(rbvectors(from, to, by)) curve ("rbVector") in (benchmarkFunctionPseudoRandom(_, 53426))
+                    using(rrbvectors(from, to, by)) curve ("rrbVector") in (benchmarkFunctionPseudoRandom(_, 53426))
                 }
             }
         }
