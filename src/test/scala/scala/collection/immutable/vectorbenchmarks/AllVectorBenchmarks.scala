e31224cdbd66add80950b70277a94b63e8512170
nicolasstucki <nicolas.stucki@gmail.com>
2014-09-29 17:01:19 +0200

    Added concatenation benchmarks

--- src/test/scala/scala/collection/immutable/vectorbenchmarks/AllVectorBenchmarks.scala
+++ src/test/scala/scala/collection/immutable/vectorbenchmarks/AllVectorBenchmarks.scala
@@ -9,6 +9,7 @@ class AllVectorBenchmarks extends PerformanceTest.OfflineReport {
     include[VectorAppendBenchmarks]
     include[VectorApplyBenchmarks]
     include[VectorBuilderBenchmarks]
+    include[VectorConcatBenchmarks]
     include[VectorIterationBenchmarks]
 
 }
