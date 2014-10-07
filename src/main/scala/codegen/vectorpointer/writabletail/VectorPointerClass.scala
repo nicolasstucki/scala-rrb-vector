f5e66ae67cc9ab5d869fbf7ef9da47c76bb4d2cd
nicolasstucki <nicolas.stucki@gmail.com>
2014-10-07 15:59:24 +0200

    Fixed bug

--- src/main/scala/codegen/vectorpointer/writabletail/VectorPointerClass.scala
+++ src/main/scala/codegen/vectorpointer/writabletail/VectorPointerClass.scala
@@ -1,11 +1,13 @@
-package codegen.vectorpointer.writabletail
+package codegen
+package vectorpointer
+package writabletail
 
 import codegen.VectorPackage
 
 import scala.annotation.tailrec
 import scala.reflect.runtime.universe._
 
-trait VectorPointerClass extends VectorPointerClass {
+trait VectorPointerClass extends vectorpointer.VectorPointerClass {
     self: VectorPackage with VectorPointerCode =>
 
 
