7afca3c22bc3a06f462b81c083181db64beb4f1e
nicolasstucki <nicolas.stucki@gmail.com>
2014-10-16 15:26:08 +0200

    Added benchmark for generated vector

--- src/test/scala/scala/collection/immutable/vectorbenchmarks/RunBenchmarks.scala
+++ src/test/scala/scala/collection/immutable/vectorbenchmarks/RunBenchmarks.scala
@@ -18,42 +18,42 @@ class RunAllBenchmarks extends PerformanceTest.OfflineRegressionReport {
 class RunAppendBenchmarks extends PerformanceTest.OfflineRegressionReport {
     include[vector.VectorAppendIntBenchmark]
     include[rrbvector.RRBVectorAppendIntBenchmark]
-    include[generated.rrbvector.closedblocks.GenRRBVector1AppendIntBenchmark]
+    include[generated.rrbvector.closedblocks.GenRRBVectorClosedBlocksAppendIntBenchmark]
 
 }
 
 class RunApplyBenchmarks extends PerformanceTest.OfflineRegressionReport {
     include[vector.VectorApplyIntBenchmark]
     include[rrbvector.RRBVectorApplyIntBenchmark]
-    include[generated.rrbvector.closedblocks.GenRRBVector1ApplyIntBenchmark]
-//    include[generated.rrbvector.fullblocks.GenRRBVector2ApplyIntBenchmark]
+    include[generated.rrbvector.closedblocks.GenRRBVectorClosedBlocksApplyIntBenchmark]
 }
 
 class RunBuilderBenchmarks extends PerformanceTest.OfflineRegressionReport {
     include[vector.VectorBuilderIntBenchmark]
     include[rrbvector.RRBVectorBuilderIntBenchmark]
-    include[generated.rrbvector.closedblocks.GenRRBVector1BuilderIntBenchmark]
+    include[generated.rrbvector.closedblocks.GenRRBVectorClosedBlocksBuilderIntBenchmark]
 }
 
 class RunConcatenationBenchmarks extends PerformanceTest.OfflineRegressionReport {
     include[vector.VectorConcatenationIntBenchmark]
     include[rrbvector.RRBVectorConcatenationIntBenchmark]
+    include[generated.rrbvector.closedblocks.GenRRBVectorClosedBlocksConcatenationIntBenchmark]
 }
 
 class RunIterationBenchmarks extends PerformanceTest.OfflineRegressionReport {
     include[vector.VectorIterationIntBenchmark]
     include[rrbvector.RRBVectorIterationIntBenchmark]
-    include[generated.rrbvector.closedblocks.GenRRBVector1IterationIntBenchmark]
+    include[generated.rrbvector.closedblocks.GenRRBVectorClosedBlocksIterationIntBenchmark]
 }
 
 class RunSplitBenchmarks extends PerformanceTest.OfflineRegressionReport {
     include[vector.VectorSplitIntBenchmark]
     include[rrbvector.RRBVectorSplitIntBenchmark]
-    include[generated.rrbvector.closedblocks.GenRRBVector1SplitIntBenchmark]
+    include[generated.rrbvector.closedblocks.GenRRBVectorClosedBlocksSplitIntBenchmark]
 }
 
 class RunMemoryAllocation extends PerformanceTest.OfflineRegressionReport {
     include[vector.VectorIntMemoryAllocation]
     include[rrbvector.RRBVectorIntMemoryAllocation]
-    include[generated.rrbvector.closedblocks.GenRRBVector1IntMemoryAllocationBenchmark]
+    include[generated.rrbvector.closedblocks.GenRRBVectorClosedBlocksIntMemoryAllocationBenchmark]
 }
