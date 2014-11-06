28e490b14411f9af1ffc339f86e0a47ef5a14add
nicolasstucki <nicolas.stucki@gmail.com>
2014-11-06 20:59:34 +0100

    Added generated code

---
+++ src/test/scala/scala/collection/immutable/vectortests/generated/rrbvector/RRBVector_complete_directLevel_32_splithalfTest.scala
@@ -0,0 +1,23 @@
+package scala {
+  package collection {
+    package immutable {
+      package vectortests {
+        package generated {
+          package rrbvector {
+            import scala.collection.immutable.vectorutils.generated.rrbvector._
+
+            import scala.collection.immutable.vectortests._
+
+            import scala.collection.immutable.vectorutils._
+
+            abstract class RRBVector_complete_directLevel_32_splithalfTest[A] extends VectorSpec[A] with RRBVector_complete_directLevel_32_splithalfGenerator[A]
+
+            class IntRRBVector_complete_directLevel_32_splithalfTest extends RRBVector_complete_directLevel_32_splithalfTest[Int] with VectorGeneratorType.IntGenerator
+
+            class StringRRBVector_complete_directLevel_32_splithalfTest extends RRBVector_complete_directLevel_32_splithalfTest[String] with VectorGeneratorType.StringGenerator
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/test/scala/scala/collection/immutable/vectorutils/generated/rrbvector/RRBVector_complete_incrementalLevel_128_splitbalancedGenerator.scala
@@ -0,0 +1,25 @@
+package scala {
+  package collection {
+    package immutable {
+      package vectorutils {
+        package generated {
+          package rrbvector {
+            trait RRBVector_complete_incrementalLevel_128_splitbalancedGenerator[A] extends BaseVectorGenerator[A] {
+              import scala.collection.immutable.generated.rrbvector._;
+              override type Vec = RRBVector_complete_incrementalLevel_128_splitbalanced[A];
+              final def vectorClassName: String = "RRBVector_complete_incrementalLevel_128_splitbalanced";
+              final override def tabulatedVector(n: Int): Vec = RRBVector_complete_incrementalLevel_128_splitbalanced.tabulate(n)(element);
+              final override def rangedVector(start: Int, end: Int): Vec = RRBVector_complete_incrementalLevel_128_splitbalanced.range(start, end).map(element);
+              final override def emptyVector: Vec = RRBVector_complete_incrementalLevel_128_splitbalanced.empty[A];
+              final override def plus(vec: Vec, elem: A): Vec = vec.:+(elem);
+              final override def plus(elem: A, vec: Vec): Vec = vec.+:(elem);
+              final override def plusPlus(vec1: Vec, vec2: Vec): Vec = vec1.++(vec2);
+              final override def take(vec: Vec, n: Int): Vec = vec.take(n);
+              final override def drop(vec: Vec, n: Int): Vec = vec.drop(n)
+            }
+          }
+        }
+      }
+    }
+  }
+}
