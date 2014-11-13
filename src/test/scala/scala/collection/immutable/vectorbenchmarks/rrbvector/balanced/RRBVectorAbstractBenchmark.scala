0a844191ab174b453579031edcf0f6e43bac16bd
nicolasstucki <nicolas.stucki@gmail.com>
2014-11-13 16:10:37 +0100

    Added check if assertions are on

--- src/test/scala/scala/collection/immutable/vectorbenchmarks/rrbvector/balanced/RRBVectorAbstractBenchmark.scala
+++ src/test/scala/scala/collection/immutable/vectorbenchmarks/rrbvector/balanced/RRBVectorAbstractBenchmark.scala
@@ -1,9 +1,11 @@
 package scala.collection.immutable.vectorbenchmarks.rrbvector.balanced
 
+import scala.collection.immutable.rrbvector.RRBVector
 import scala.collection.immutable.vectorbenchmarks.BaseVectorBenchmark
 import scala.collection.immutable.vectorutils.BaseVectorGenerator.RRBVectorGenerator
 
-trait RRBVectorAbstractBenchmark [A] extends BaseVectorBenchmark[A] with RRBVectorGenerator[A] {
+trait RRBVectorAbstractBenchmark[A] extends BaseVectorBenchmark[A] with RRBVectorGenerator[A] {
     override def generateVectors(from: Int, to: Int, by: Int) = sizes(from, to, by).map(((size) => tabulatedVector(size)));
-    override def vectorName: String = super.vectorName.+("Balanced")
+
+    override def vectorName: String = if (RRBVector.compileAssertions) throw new IllegalStateException("RRBVector.compileAssertions must be false to run benchmarks.") else super.vectorName.+("Balanced")
 }
--- src/test/scala/scala/collection/immutable/vectorbenchmarks/rrbvector/xunbalanced/RRBVectorAbstractBenchmark.scala
+++ src/test/scala/scala/collection/immutable/vectorbenchmarks/rrbvector/xunbalanced/RRBVectorAbstractBenchmark.scala
@@ -1,9 +1,11 @@
 package scala.collection.immutable.vectorbenchmarks.rrbvector.xunbalanced
 
+import scala.collection.immutable.rrbvector.RRBVector
 import scala.collection.immutable.vectorbenchmarks.BaseVectorBenchmark
 import scala.collection.immutable.vectorutils.BaseVectorGenerator.RRBVectorGenerator
 
-trait RRBVectorAbstractBenchmark [A] extends BaseVectorBenchmark[A] with RRBVectorGenerator[A] {
+trait RRBVectorAbstractBenchmark[A] extends BaseVectorBenchmark[A] with RRBVectorGenerator[A] {
     override def generateVectors(from: Int, to: Int, by: Int) = sizes(from, to, by).map(((size) => randomVectorOfSize(size)(defaultVectorConfig(111))));
-    override def vectorName: String = super.vectorName.+("XUnbalanced")
+
+    override def vectorName: String = if (RRBVector.compileAssertions) throw new IllegalStateException("RRBVector.compileAssertions must be false to run benchmarks.") else super.vectorName.+("XUnbalanced")
 }
