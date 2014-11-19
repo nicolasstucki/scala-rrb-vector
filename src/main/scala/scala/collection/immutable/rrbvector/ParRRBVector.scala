c9fcb3d3307987ff69287937eb2aefb890dbbf90
nicolasstucki <nicolas.stucki@gmail.com>
2014-11-19 11:36:45 +0100

    Fixed bug with parallel splitter

--- src/main/scala/scala/collection/immutable/rrbvector/ParRRBVector.scala
+++ src/main/scala/scala/collection/immutable/rrbvector/ParRRBVector.scala
@@ -4,6 +4,9 @@ package parallel.immutable
 package rrbvector
 
 import scala.collection.generic.{GenericParTemplate, CanCombineFrom, ParFactory}
+import scala.collection.immutable.VectorBuilder
+import scala.collection.mutable
+import scala.collection.parallel.immutable.LazyParVectorCombiner
 import scala.collection.parallel.{ParSeqLike, Combiner, SeqSplitter}
 import mutable.ArrayBuffer
 import immutable.rrbvector.RRBVector
@@ -33,7 +36,7 @@ class ParRRBVector[+T](private[this] val vector: RRBVector[T])
 
     override def toVector: Vector[T] = vector.toVector
 
-    class ParRRBVectorSplitter(_start: Int, _end: Int) extends RRBVectorIterator[T](_start, _end) with SeqSplitter[T] {
+    class ParRRBVectorSplitter(val _start: Int, val _end: Int) extends RRBVectorIterator[T](_start, _end) with SeqSplitter[T] {
         override def remaining: Int = super.remaining
 
         def dup: SeqSplitter[T] = {
@@ -45,24 +48,19 @@ class ParRRBVector[+T](private[this] val vector: RRBVector[T])
         def split: Seq[ParRRBVectorSplitter] = {
             val rem = remaining
             if (rem >= 2) {
-                val splitSize =
-                    if (rem < 32) 1
-                    else if (rem < 1024) 1 << 5
-                    else if (rem < 32768) 1 << 10
-                    else if (rem < 1048576) 1 << 15
-                    else if (rem < 33554432) 1 << 20
+                val _half = rem / 2
+                val _splitModulo =
+                    if (rem <= (1 << 5)) 1
+                    else if (rem <= (1 << 10)) 1 << 5
+                    else if (rem <= (1 << 15)) 1 << 10
+                    else if (rem <= (1 << 20)) 1 << 15
+                    else if (rem <= (1 << 25)) 1 << 20
                     else 1 << 25
-                val splitted = new ArrayBuffer[ParRRBVectorSplitter]
-                var currentPos = _end - remaining
-                while (currentPos < rem) {
-                    val pit = new ParRRBVectorSplitter(currentPos, math.min(currentPos + splitSize, _end))
-                    pit.initIteratorFrom(this)
-                    splitted += pit
-                    currentPos += splitSize
-                }
-                splitted
+                val _halfAdjusted = if (_half > _splitModulo) _half - _half % _splitModulo else if (_splitModulo < _end) _splitModulo else _half
+                psplit(_halfAdjusted, rem - _halfAdjusted)
+            } else {
+                Seq(this)
             }
-            else Seq(this)
         }
 
 
@@ -89,6 +87,7 @@ object ParRRBVector extends ParFactory[ParRRBVector] {
     def newBuilder[T]: Combiner[T, ParRRBVector[T]] = newCombiner[T]
 
     def newCombiner[T]: Combiner[T, ParRRBVector[T]] = new ParRRBVectorCombiner[T]
+    //        def newCombiner[T]: Combiner[T, ParRRBVector[T]] = new LazyParRRBVectorCombiner[T]
 }
 
 private[immutable] class ParRRBVectorCombiner[T] extends Combiner[T, ParRRBVector[T]] {
@@ -112,11 +111,48 @@ private[immutable] class ParRRBVectorCombiner[T] extends Combiner[T, ParRRBVecto
     }
 
     def combine[U <: T, NewTo >: ParRRBVector[T]](other: Combiner[U, NewTo]) = {
-        if (other eq this) this
+        if (this eq other) this
         else {
-            val that = other.asInstanceOf[ParRRBVectorCombiner[T]]
-            builder ++= that.builder.result()
-            this
+            val newCombiner = new ParRRBVectorCombiner[T]
+            newCombiner ++= this.builder.result()
+            newCombiner ++= other.asInstanceOf[ParRRBVectorCombiner[T]].builder.result()
+            newCombiner
         }
+    }
+}
+
+
+private[immutable] class LazyParRRBVectorCombiner[T] extends Combiner[T, ParRRBVector[T]] {
+    var sz = 0
+    val vectors = new ArrayBuffer[RRBVectorBuilder[T]] += new RRBVectorBuilder[T]
+
+    def size: Int = sz
+
+    def +=(elem: T): this.type = {
+        vectors.last += elem
+        sz += 1
+        this
+    }
+
+    def clear() = {
+        vectors.clear()
+        vectors += new RRBVectorBuilder[T]
+        sz = 0
+    }
+
+    def result(): ParRRBVector[T] = {
+        val rvb = new RRBVectorBuilder[T]
+        for (vb <- vectors) {
+            rvb ++= vb.result
+        }
+        new ParRRBVector(rvb.result())
+    }
+
+    def combine[U <: T, NewTo >: ParRRBVector[T]](other: Combiner[U, NewTo]) = if (other eq this) this
+    else {
+        val that = other.asInstanceOf[LazyParRRBVectorCombiner[T]]
+        sz += that.sz
+        vectors ++= that.vectors
+        this
     }
 }
