5c3f479160cf34030ec11d3204a372236153163d
nicolasstucki <nicolas.stucki@gmail.com>
2014-11-21 14:29:37 +0100

    Updated parallel benchmark support

--- src/test/scala/scala/collection/immutable/vectorbenchmarks/genericbenchmarks/ParMapBenchmarks.scala
+++ src/test/scala/scala/collection/immutable/vectorbenchmarks/genericbenchmarks/ParMapBenchmarks.scala
@@ -3,25 +3,6 @@ package scala.collection.immutable.vectorbenchmarks.genericbenchmarks
 import org.scalameter.{Key, PerformanceTest}
 
 import scala.collection.immutable.vectorbenchmarks.BaseVectorBenchmark
-import scala.collection.parallel.ForkJoinTaskSupport
-
-object ParSupport {
-
-    lazy val pool1 = new scala.concurrent.forkjoin.ForkJoinPool(1)
-    lazy val pool2 = new scala.concurrent.forkjoin.ForkJoinPool(2)
-    lazy val pool4 = new scala.concurrent.forkjoin.ForkJoinPool(4)
-    lazy val pool8 = new scala.concurrent.forkjoin.ForkJoinPool(8)
-    lazy val pool16 = new scala.concurrent.forkjoin.ForkJoinPool(16)
-
-    def getTaskSupport(n: Int) = n match {
-        case 1 => new ForkJoinTaskSupport(pool1)
-        case 2 => new ForkJoinTaskSupport(pool2)
-        case 4 => new ForkJoinTaskSupport(pool4)
-        case 8 => new ForkJoinTaskSupport(pool8)
-        case 16 => new ForkJoinTaskSupport(pool16)
-    }
-
-}
 
 abstract class ParMapBenchmarks[A] extends BaseVectorBenchmark[A] {
     self: PerformanceTest =>
