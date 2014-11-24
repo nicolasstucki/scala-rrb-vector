d62c49cb0926a77a62b912f785b47ff35330964d
nicolasstucki <nicolas.stucki@gmail.com>
2014-11-24 15:52:51 +0100

    Changed the prepend benchmark to show the amortization of the prepends in all the configurations.

--- src/test/scala/scala/collection/immutable/vectorbenchmarks/genericbenchmarks/PrependBenchmarks.scala
+++ src/test/scala/scala/collection/immutable/vectorbenchmarks/genericbenchmarks/PrependBenchmarks.scala
@@ -19,40 +19,13 @@ abstract class PrependBenchmarks[A] extends BaseVectorBenchmark[A] {
           Key.exec.minWarmupRuns -> 2000,
           Key.exec.maxWarmupRuns -> 5000
           ) in {
-            val times = 5000
+            val times = 500
 
-            performance of s"prepend 1 element, $times times" in {
-
-                performance of s"Height $height" in {
-                    using(generateVectors(from, to, by)) curve vectorName in { vec =>
-                        sideeffect = prepend(vec, 1, times)
-                    }
-                }
-            }
-
-            performance of s"prepend 8 element, $times times" in {
+            performance of s"prepend 256 elements, ${times} times" in {
 
                 performance of s"Height $height" in {
                     using(generateVectors(from, to, by)) curve vectorName in { vec =>
-                        sideeffect = prepend(vec, 8, times)
-                    }
-                }
-            }
-
-            performance of s"prepend 32 elements, $times times" in {
-
-                performance of s"Height $height" in {
-                    using(generateVectors(from, to, by)) curve vectorName in { vec =>
-                        sideeffect = prepend(vec, 32, times)
-                    }
-                }
-            }
-
-            performance of s"prepend 100 elements, ${times / 10} times" in {
-
-                performance of s"Height $height" in {
-                    using(generateVectors(from, to, by)) curve vectorName in { vec =>
-                        sideeffect = prepend(vec, 100, times / 10)
+                        sideeffect = prepend(vec, 256, times)
                     }
                 }
             }
