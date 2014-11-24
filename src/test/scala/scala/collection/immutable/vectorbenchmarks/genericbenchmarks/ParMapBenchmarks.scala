9f51e8b57042df9ec72712261c1a1afdb645e6df
nicolasstucki <nicolas.stucki@gmail.com>
2014-11-24 13:08:07 +0100

    Added unbalanced parallel map workload benchmark

--- src/test/scala/scala/collection/immutable/vectorbenchmarks/genericbenchmarks/ParMapBenchmarks.scala
+++ src/test/scala/scala/collection/immutable/vectorbenchmarks/genericbenchmarks/ParMapBenchmarks.scala
@@ -10,15 +10,18 @@ abstract class ParMapBenchmarks[A] extends BaseVectorBenchmark[A] {
     def mapSelfFun(x: A): A
 
     def mapBenchFun(x: A): A
+    def mapBenchFun2(x: A): A
 
     performanceOfVectors { height =>
         val (from, to, by) = fromToBy(height)
 
         var sideeffect = 0
 
+        val minWarmupRuns0 = if (height >= 4) 15 else if (height >= 3) 30 else 300
+        val maxWarmupRuns0 = if (height >= 4) 40 else if (height >= 3) 80 else 800
         measure method "map" config(
-          Key.exec.minWarmupRuns -> 300,
-          Key.exec.maxWarmupRuns -> 800
+          Key.exec.minWarmupRuns -> minWarmupRuns0,
+          Key.exec.maxWarmupRuns -> maxWarmupRuns0
           ) in {
             val times = 100
 
@@ -53,10 +56,11 @@ abstract class ParMapBenchmarks[A] extends BaseVectorBenchmark[A] {
 
         }
 
-        val minWarmupRuns = if (height >= 3) 30 else 100
+        val minWarmupRuns1 = if (height >= 4) 10 else if (height >= 3) 30 else 100
+        val maxWarmupRuns1 = if (height >= 4) 25 else if (height >= 3) 60 else 150
         measure method "map" config(
-          Key.exec.minWarmupRuns -> minWarmupRuns,
-          Key.exec.maxWarmupRuns -> 150
+          Key.exec.minWarmupRuns -> minWarmupRuns1,
+          Key.exec.maxWarmupRuns -> maxWarmupRuns1
           ) in {
             performance of s"map into mapBencFun" in {
                 performance of s"Height $height" in {
@@ -73,6 +77,36 @@ abstract class ParMapBenchmarks[A] extends BaseVectorBenchmark[A] {
                                 val parvec = vec.par
                                 parvec.tasksupport = ParSupport.getTaskSupport(threadPoolSize)
                                 sideeffect = (parvec map mapBenchFun).length
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        val minWarmupRuns2 = if (height >= 3) 30 else 150
+        val maxWarmupRuns2 = if (height >= 3) 150 else 200
+        if (height <= 3) {
+            measure method "map" config(
+              Key.exec.minWarmupRuns -> minWarmupRuns2,
+              Key.exec.maxWarmupRuns -> maxWarmupRuns2
+              ) in {
+                performance of s"map into mapBencFun2" in {
+                    performance of s"Height $height" in {
+                        using(generateVectors(from, to, by)) curve vectorName in { vec =>
+                            sideeffect = (vec map mapBenchFun2).length
+                        }
+                    }
+                }
+                for (threadPoolSize <- Seq(1, 2, 4, 8, 16)) {
+                    performance of s"par.map into mapBencFun2" in {
+                        performance of s"$threadPoolSize threads in pool" in {
+                            performance of s"Height $height" in {
+                                using(generateVectors(from, to, by)) curve vectorName in { vec =>
+                                    val parvec = vec.par
+                                    parvec.tasksupport = ParSupport.getTaskSupport(threadPoolSize)
+                                    sideeffect = (parvec map mapBenchFun2).length
+                                }
                             }
                         }
                     }
--- src/test/scala/scala/collection/immutable/vectorutils/VectorGeneratorType.scala
+++ src/test/scala/scala/collection/immutable/vectorutils/VectorGeneratorType.scala
@@ -9,6 +9,18 @@ trait VectorGeneratorType[A] {
     protected final def mapBenchFunCompute(x: Int): Int = {
         Math.cos(3.1415 + Math.cos(1.0 + Math.sin(2.0 + Math.cos(Math.sin(Math.sin(Math.cos(x + 1.0))))))).toInt
     }
+
+    protected final def mapBenchFun2Compute(x: Int): Int = {
+        // This is a inefficient implementation of prime testing
+        // that aims to generate unbalanced workloads
+        assert(x >= 0)
+        for (i <- 2 until (x + 2)) {
+            if (x % i == 0) {
+                return i
+            }
+        }
+        x
+    }
 }
 
 object VectorGeneratorType {
@@ -21,6 +33,10 @@ object VectorGeneratorType {
 
         final def mapBenchFun(x: Int) = {
             mapBenchFunCompute(1)
+        }
+
+        final def mapBenchFun2(x: Int): Int = {
+            mapBenchFun2Compute(x)
         }
 
         final def mapFun1(x: Int): Int = x % 3
@@ -40,7 +56,16 @@ object VectorGeneratorType {
         final def mapSelfFun(x: String) = x
 
         final def mapBenchFun(x: String) = {
-            mapBenchFunCompute(1).toString
+            mapBenchFunCompute(x.hashCode).toString
+        }
+
+        final def mapBenchFun2(x: String): String = {
+            val y = try {
+                Integer.parseInt(x)
+            } catch {
+                case _: NumberFormatException => x.hashCode
+            }
+            mapBenchFun2Compute(y).toString
         }
 
         final def mapFun1(x: String): String = {
