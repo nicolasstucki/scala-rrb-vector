4b98405bfef393ab99060ba96815238a0c06d1bc
nicolasstucki <nicolas.stucki@gmail.com>
2014-11-27 22:43:16 +0100

    Calibration

--- src/test/scala/scala/collection/immutable/vectorbenchmarks/genericbenchmarks/ConcatenationBenchmarks.scala
+++ src/test/scala/scala/collection/immutable/vectorbenchmarks/genericbenchmarks/ConcatenationBenchmarks.scala
@@ -14,7 +14,7 @@ abstract class ConcatenationBenchmarks[A] extends BaseVectorBenchmark[A] {
 
         var sideeffect = 0
 
-        val warmups = 1000
+        val warmups = 2000
 
         performance of "concatenation" config(
           Key.exec.minWarmupRuns -> warmups,
--- src/test/scala/scala/collection/immutable/vectorbenchmarks/vector/VectorConcatenationBenchmark.scala
+++ src/test/scala/scala/collection/immutable/vectorbenchmarks/vector/VectorConcatenationBenchmark.scala
@@ -5,7 +5,7 @@ import scala.collection.immutable.vectorutils.VectorGeneratorType
 
 abstract class VectorAbstractConcatenationBenchmark[A] extends ConcatenationBenchmarks[A] with VectorBenchmark[A] {
     // Used in immutable.vector to bound the sizes
-    override def to(n: Int): Int = math.min(n, 8200)
+    override def to(n: Int): Int = math.min(n, 16000)
 }
 
 class VectorConcatenationIntBenchmark extends VectorAbstractConcatenationBenchmark[Int] with VectorGeneratorType.IntGenerator
