cb86ebb5e0e1164cbec65e14bbea584db26be195
nicolasstucki <nicolas.stucki@gmail.com>
2014-12-07 11:16:00 +0100

    Removed generated vectors

--- src/test/scala/scala/collection/immutable/vectortests/QuickTest.scala
+++ src/test/scala/scala/collection/immutable/vectortests/QuickTest.scala
@@ -2,20 +2,23 @@ package scala.collection.immutable.vectortests
 
 import scala.collection.immutable.rrbvector.{RRBVectorIterator, RRBVector}
 import scala.collection.immutable.vectorutils.{BaseVectorGenerator, VectorGeneratorType}
-import scala.collection.immutable.vectorutils.generated.rrbvector._
+//import scala.collection.immutable.vectorutils.generated.rrbvector._
 
 
 object QuickTest extends App {
 
-    val n = 32768
-    val seed = 111
+//    val n = 32768
+//    val seed = 111
+//
+//    def f(x: Int) = -x
+//
+//    val vector = (new BaseVectorGenerator.RRBVectorGenerator[Int] with VectorGeneratorType.IntGenerator).randomVectorOfSize(n)(BaseVectorGenerator.defaultVectorConfig(seed))
+//    val parVec = vector.par map f
+//
+//    println(vector)
+//    println(parVec)
 
-    def f(x: Int) = -x
-
-    val vector = (new BaseVectorGenerator.RRBVectorGenerator[Int] with VectorGeneratorType.IntGenerator).randomVectorOfSize(n)(BaseVectorGenerator.defaultVectorConfig(seed))
-    val parVec = vector.par map f
-
-    println(vector)
-    println(parVec)
+    val vec = RRBVector.range(0, 32767)
+    println(vec)
 
 }
